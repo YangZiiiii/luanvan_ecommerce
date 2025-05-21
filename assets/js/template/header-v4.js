0200a63e42eb40680c3fa715393ebf448bd9aa97
tantiensucana <tantien270320@gmail.com>
2025-05-22 05:33:46 +0700

    login user

--- assets/js/template/header-v4.js
+++ assets/js/template/header-v4.js
@@ -136,6 +136,8 @@ class AppHeaderV4 extends HTMLElement {
 
                </div>
                <!-- Icon header -->
+               <!-- HTML for Cart -->
+
                <div class="wrap-icon-header flex-w flex-r-m">
                   <div class="flex-c-m stext-106 cl6 size-105 pointer trans-04 m-tb-4 js-show-search box-search">
                      <i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
@@ -152,7 +154,7 @@ class AppHeaderV4 extends HTMLElement {
 
                   <div class="header__cart-container">
                      <div
-                        class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart noti-none"
+                        class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart"
                         data-notify="0" id="cart-icon-header">
                         <i class="zmdi zmdi-shopping-cart "></i>
                      </div>
@@ -174,93 +176,6 @@ class AppHeaderV4 extends HTMLElement {
 
             </nav>
          </div>
-      </div>
-
-      <!-- Header Mobile -->
-      <div class="wrap-header-mobile">
-         <!-- Logo moblie -->
-         <div class="logo-mobile">
-            <a href="index.html"><img src="./assets/images/icons/logo-01.png" alt="IMG-LOGO"></a>
-         </div>
-
-         <!-- Icon header -->
-         <div class="wrap-icon-header flex-w flex-r-m m-r-15">
-            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
-               <i class="zmdi zmdi-search"></i>
-            </div>
-
-            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart"
-               data-notify="2">
-               <i class="zmdi zmdi-shopping-cart"></i>
-            </div>
-
-            <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti"
-               data-notify="0">
-               <i class="zmdi zmdi-favorite-outline"></i>
-            </a>
-         </div>
-
-         <!-- Button show menu -->
-         <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
-            <span class="hamburger-box">
-               <span class="hamburger-inner"></span>
-            </span>
-         </div>
-      </div>
-
-
-      <!-- Menu Mobile -->
-      <div class="menu-mobile">
-         <ul class="topbar-mobile">
-            <li>
-               <div class="left-top-bar">
-                  Free shipping for standard order over $100
-               </div>
-            </li>
-
-            <li>
-               <div class="right-top-bar flex-w h-full">
-                  <a href="#" class="flex-c-m p-lr-10 trans-04">
-                     Help & FAQs
-                  </a>
-
-                  <a href="#" class="flex-c-m p-lr-10 trans-04">
-                     My Account
-                  </a>
-
-                  <a href="#" class="flex-c-m p-lr-10 trans-04">
-                     EN
-                  </a>
-
-                  <a href="#" class="flex-c-m p-lr-10 trans-04">
-                     USD
-                  </a>
-               </div>
-            </li>
-         </ul>
-
-         <ul class="main-menu-m">
-            <li>
-               <a href="index.html">Home</a>
-            </li>
-
-            <li>
-               <a href="product.html">Shop</a>
-            </li>
-
-            <li>
-               <a href="shoppingCart.html" class="label1 rs1" data-label1="hot">Features</a>
-            </li>
-            s
-
-            <li>
-               <a href="about.html">About</a>
-            </li>
-
-            <li>
-               <a href="contact.html">Contact</a>
-            </li>
-         </ul>
       </div>
 
       <!-- Modal Search -->
--- assets/js/template/header.js
+++ assets/js/template/header.js
@@ -19,6 +19,17 @@ class AppHeader extends HTMLElement {
                   <a href="#" class="flex-c-m trans-04 p-lr-25 open-modal-log p-l-sign" data-tab="register">
                      Sign up
                   </a>
+                  <div class="login-menu-container">
+                  <a href="#" class="none-underline flex-c-m trans-04 p-lr-25 login-menu-toggle" style="padding: 0 10px; height: 100%;">
+                     <i class="fa fa-user"></i> <span class="ml-2">Username</span>
+                  </a>
+
+                  <div class="sub-menu-login">
+                     <a href="loginInfo.html" class="sub-menu-login-item">Thông tin cá nhân</a>
+                     <a href="/orders" class="sub-menu-login-item">Lịch sử mua hàng</a>
+                     <a href="logout.html" class="sub-menu-login-item">Đăng xuất</a>
+                  </div>
+               </div>
                </div>
             </div>
          </div>
@@ -137,6 +148,7 @@ class AppHeader extends HTMLElement {
 
                </div>
                <!-- Icon header -->
+               <!-- Change for cart -->
                <div class="wrap-icon-header flex-w flex-r-m">
                   <div class="flex-c-m stext-106 cl6 size-105 pointer trans-04 m-tb-4 js-show-search box-search">
                      <i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
@@ -170,92 +182,7 @@ class AppHeader extends HTMLElement {
       </div>
       </div>
 
-      <!-- Header Mobile -->
-      <div class="wrap-header-mobile">
-         <!-- Logo moblie -->
-         <div class="logo-mobile">
-            <a href="index.html"><img src="./assets/images/icons/logo-01.png" alt="IMG-LOGO" /></a>
-         </div>
-
-         <!-- Icon header -->
-         <!-- Change for cart -->
-         <div class="wrap-icon-header flex-w flex-r-m m-r-15">
-            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
-               <i class="zmdi zmdi-search"></i>
-            </div>
-
-            <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart"
-               data-notify="2">
-               <i class="zmdi zmdi-shopping-cart"></i>
-            </div>
-
-            <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti"
-               data-notify="0">
-               <i class="zmdi zmdi-favorite-outline"></i>
-            </a>
-         </div>
-
-         <!-- Button show menu -->
-         <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
-            <span class="hamburger-box">
-               <span class="hamburger-inner"></span>
-            </span>
-         </div>
-      </div>
-
-      <!-- Menu Mobile -->
-      <div class="menu-mobile">
-         <ul class="topbar-mobile">
-            <li>
-               <div class="left-top-bar">
-                  Free shipping for standard order over $100
-               </div>
-            </li>
-
-            <li>
-               <div class="right-top-bar flex-w h-full">
-                  <a href="#" class="flex-c-m p-lr-10 trans-04">
-                     Help & FAQs
-                  </a>
-
-                  <a href="#" class="flex-c-m p-lr-10 trans-04">
-                     My Account
-                  </a>
-
-                  <a href="#" class="flex-c-m p-lr-10 trans-04"> EN </a>
-
-                  <a href="#" class="flex-c-m p-lr-10 trans-04"> USD </a>
-               </div>
-            </li>
-         </ul>
-
-         <ul class="main-menu-m">
-            <li>
-               <a href="index.html">Home</a>
-
-               <span class="arrow-main-menu-m">
-                  <i class="fa fa-angle-right" aria-hidden="true"></i>
-               </span>
-            </li>
-
-            <li>
-               <a href="product.html">Shop</a>
-            </li>
-
-            <li>
-               <a href="shoppingCart.html" class="label1 rs1" data-label1="hot">Features</a>
-            </li>
-
-
-            <li>
-               <a href="about.html">About</a>
-            </li>
-
-            <li>
-               <a href="contact.html">Contact</a>
-            </li>
-         </ul>
-      </div>
+      
 
       <!-- Modal Search -->
       <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
